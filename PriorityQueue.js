55a173d852ac04305fa426a26864b9a342a073c9
Filiponesco <fifigaw98@gmail.com>
2020-01-14 15:27:26 +0100

    add presentation, remove unneccesary code

--- PriorityQueue.js
+++ PriorityQueue.js
@@ -3,15 +3,13 @@ class PriorityQueue {
     constructor() {
         this.items = [];
     }
-    // enqueue function to add element 
-    // to the queue as per priority 
+    // enqueue function to add element to the queue as per priority 
     enqueue(element) {
         var contain = false;
 
         for (var i = 0; i < this.items.length; i++) {
             if (this.items[i].priority > element.priority) {
-                // Once the correct location is found it is 
-                // enqueued 
+                // Once the correct location is found it is enqueued 
                 this.items.splice(i, 0, element);
                 contain = true;
                 break;
@@ -21,8 +19,7 @@ class PriorityQueue {
             this.items.push(element);
         }
     }
-    // dequeue method to remove 
-    // element from the queue 
+    // dequeue method to remove element from the queue 
     dequeue() {
         if (this.isEmpty()){
             alert("Underflow");
@@ -30,9 +27,7 @@ class PriorityQueue {
         }
         return this.items.shift();
     }
-    // returns the highest priority element
-    // highest means the most highest priority is 0
-    // in the Priority queue without removing it. 
+    // returns the highest priority element highest. Means the most highest priority is 0
     front() {
         if (this.isEmpty())
             return "No elements in Queue";
@@ -45,8 +40,7 @@ class PriorityQueue {
         alert("No path!");
         return "No path!";
     }
-    // returns the lowest priorty 
-    // element of the queue 
+    // returns the lowest priorty element of the queue 
     rear() {
         if (this.isEmpty())
             return "No elements in Queue";
@@ -55,12 +49,6 @@ class PriorityQueue {
     isEmpty() {
         // return true if the queue is empty. 
         return this.items.length == 0;
-    }
-    printPQueue() {
-        var str = "";
-        for (var i = 0; i < this.items.length; i++)
-            str += this.items[i].element + " ";
-        return str;
     }
     deleteRear(){
         this.items.pop();
